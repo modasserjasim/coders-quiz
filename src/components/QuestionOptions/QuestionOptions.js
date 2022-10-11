e41fbc9c7791d909d8005f6688157b789bca02d5
Modasserul Hoque <modasserulhaq.jasim@gmail.com>
2022-10-11 11:10:45 +0600

    handle correct/wrong answer added

--- src/components/QuestionOptions/QuestionOptions.js
+++ src/components/QuestionOptions/QuestionOptions.js
@@ -5,8 +5,9 @@ const QuestionOptions = ({ option, handleAnswer }) => {
     return (
         <div className='border-2 border-cyan-500 rounded-md p-4 m-2 flex items-center'>
             <label onClick={() => handleAnswer(option)} className="inline-flex items-center">
-                <input type="radio" className="form-radio" name="radio-sizes" value="2"></input>
+                <input type="radio" className="form-radio" name="radio-sizes" />
                 <span className="ml-3 text-md">{option}</span>
+
             </label>
         </div>
     );
--- src/components/QuizQuestion/QuizQuestion.js
+++ src/components/QuizQuestion/QuizQuestion.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import Swal from 'sweetalert2';
 import QuestionOptions from '../QuestionOptions/QuestionOptions';
 
 const QuizQuestion = ({ quizQuestion, questions }) => {
     const { id, question, correctAnswer, options } = quizQuestion;
-
     // console.log(quizQuestion);
     const handleAnswer = (option) => {
-        console.log(correctAnswer);
         if (option === correctAnswer) {
-            toast.success('Your answer is correct!')
+            toast.success('Your answer is correct!', { autoClose: 1500, toastId: id })
         } else {
-            toast.error('incorrect answer!')
+            toast.error('Oops! incorrect answer!', { autoClose: 1500, toastId: id })
         }
-        console.log(option);
-        toast('toast')
+    }
+    const handleShowAnswer = () => {
+        Swal.fire({
+            icon: 'success',
+            title: 'The currect answer is:',
+            html: correctAnswer
+        })
     }
     return (
         <div>
@@ -24,7 +28,7 @@ const QuizQuestion = ({ quizQuestion, questions }) => {
                     <div className="flex items-center text-xl font-semibold">
                         Quiz 1: {question}
                     </div>
-                    <button>
+                    <button onClick={() => handleShowAnswer()}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
                             <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
